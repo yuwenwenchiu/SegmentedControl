e07e4132a31b9a02d05cb92f6ce9be382d136ccf
Yuwen Chiu <yuwenchiu@yuwende-macbook-pro.local>
2018-08-16 23:05:01 +0800

    Initial Commit

---
+++ SegmentedControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegmentedControl
+//
+//  Created by Yuwen Chiu on 2018/8/16.
+//  Copyright © 2018年 YuwenChiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
